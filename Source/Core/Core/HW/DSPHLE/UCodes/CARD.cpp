fc680095ceb6fc3f301d9ee5de1a13ed24dabe9e
Pokechu22 <Pokechu022@gmail.com>
2022-06-04 19:10:34 -0700

    Untested basic implementation

--- Source/Core/Core/HW/DSPHLE/UCodes/CARD.cpp
+++ Source/Core/Core/HW/DSPHLE/UCodes/CARD.cpp
@@ -109,7 +109,50 @@ protected:
 static std::unique_ptr<Accelerator> s_accelerator = std::make_unique<HLEAccelerator>();
 }  // namespace
 
-static void ProcessParameters(CARDUCode::CardUcodeParameters params)
+static void DoCardHashStep(CARDUCode::CardUcodeWorkData* data, u16 prev1, u16 prev2, u16 new1,
+                           u16 new2)
+{
+  // ROM function from 86e5 to 8725
+
+  // 86e8 - 86eb
+  u16 tmp1 = (new2 << 4) | prev2;
+  // This happens due to sign extension from the arithmetic right shift at 86eb
+  if ((tmp1 & 0x80) != 0)
+    tmp1 |= 0xff00;
+  // 86ec - 86f0
+  tmp1 ^= (prev1 << 8);
+  tmp1 ^= (new1 << 12);
+
+  // Assuming the accelerator reads nybbles, this just comes out to:
+  // u16 tmp = (new1 << 12) | (prev1 << 8) | (new2 << 4) | prev2;
+  // if (new2 & 0x80) tmp ^= 0xff00;
+
+  // 86f1 - 86f7
+  data->work_0408 += tmp1;  // unsigned addition
+
+  // 86e9 - 86ea and 86f6 - 86fb
+  const u32 tmp2 = (data->work_040c ^ data->work_040e) + data->work_0408;
+
+  // 86fb - 86fd
+  data->work_0411++;
+
+  // 86fe - 870b
+  u32 rotate = (data->work_0410 + data->work_0411) & 0x1f;
+  u32 tmp3 = (tmp2 >> rotate);
+  if (rotate != 0)
+    tmp3 += (tmp2 << (0x20 - rotate));
+
+  // 86fe and 870b - 870e
+  data->work_040a += tmp3;
+
+  // 870f - 871c
+  data->work_040c = (~data->work_0408 & data->work_040a) | (data->work_0408 & data->work_040e);
+
+  // 871d - 8724
+  data->work_040e = data->work_0408 ^ data->work_040a ^ data->work_040c;
+}
+
+static void DoCardHash(CARDUCode::CardUcodeParameters params)
 {
   // Large ROM function from 8644 to 86e4
   CARDUCode::CardUcodeWorkData data{};
@@ -135,7 +178,39 @@ static void ProcessParameters(CARDUCode::CardUcodeParameters params)
 
   // 866a - 8684 - Copy from dmem to the accelerator and also sum the bytes
 
-  // s_accelerator->WriteD3();
+  u32 sum = 0;
+  for (u32 i = 0; i < (params.input_size / sizeof(u16)); i++)
+  {
+    // DRAM and most things the DSP interacts with use 16-bit words
+    const u8 first = buffer[2 * i];
+    const u8 second = buffer[(2 * i) + 1];
+    const u16 value = (u16(first) << 8) | u16(second);
+    s_accelerator->WriteD3(value);
+    sum += first;
+    sum += second;
+  }
+  if (params.input_size & 1)
+  {
+    // Handle the last byte
+    // Note that this won't go out of bounds on the buffer, as the buffer is read in groups of 4
+    // bytes (possibly a restriction on DMA sizes?).
+    // The second value is written to the accelerator just in case it makes a difference with
+    // the behavior of the mode, but this may be unnecessary. (We're writing 2-byte words to ARAM.)
+    const u8 first = buffer[params.input_size - 1];
+    const u8 second = buffer[params.input_size];
+
+    const u16 value = (u16(first) << 8) | u16(second);
+    s_accelerator->WriteD3(value);
+    sum += buffer[params.input_size - 1];
+  }
+
+  // 8685 - 86a3 - Initialize a bunch of state
+  data.work_0408 = sum + 0x170a7489;
+  data.work_040a = 0x05efe0aa;
+  data.work_040c = 0xdaf4b157;
+  data.work_040e = 0x6bbec3b6;
+  data.work_0410 = (sum + 8) & 0xffff;
+  data.work_0411 = 0;
 
   // 86a4 - 86b1 - Set up the accelerator again
   s_accelerator->SetSampleFormat(0);
@@ -144,6 +219,42 @@ static void ProcessParameters(CARDUCode::CardUcodeParameters params)
   // (mask 0x03ff'ffff) according to DSP.cpp, this indicates that format 0 reads nybbles, probably.
   s_accelerator->SetEndAddress(0x07ff'ffff);
   s_accelerator->SetCurrentAddress(params.aram_work_addr);
+
+  // 86b2 - 86d2 - Actually do the hashing
+  u16 prev1 = s_accelerator->ReadD3();
+  u16 prev2 = s_accelerator->ReadD3();
+  u16 new_counter;
+  if (params.input_size != 0)
+    new_counter = (params.input_size - 1) / 2;
+  else
+    new_counter = 0xffff;  // TODO: Does this case actually happen?
+
+  for (u32 i = 0; i < new_counter; i++)
+  {
+    // Note: in the actual ROM, the accelerator is read in DoCardHashStep, but the copy from new to
+    // prev happens outside.
+    u16 new1 = s_accelerator->ReadD3();
+    u16 new2 = s_accelerator->ReadD3();
+    DoCardHashStep(&data, prev1, prev2, new1, new2);
+    prev1 = new1;
+    prev2 = new2;
+    new1 = s_accelerator->ReadD3();
+    new2 = s_accelerator->ReadD3();
+    DoCardHashStep(&data, prev1, prev2, new1, new2);
+    prev1 = new1;
+    prev2 = new2;
+  }
+  if ((params.input_size & 1) == 0)
+  {
+    // Handle the last byte -- note that this happens for *even* counts, unlike before.
+    // I'm not sure if this is an implementation detail that actually matters.
+    u16 new1 = s_accelerator->ReadD3();
+    u16 new2 = s_accelerator->ReadD3();
+    DoCardHashStep(&data, prev1, prev2, new1, new2);
+  }
+
+  // 86d6 - 86e4 - DMA back the hash
+  HLEMemory_Write_U32(params.mram_output_addr, data.work_040a);
 }
 
 void CARDUCode::Update()
@@ -197,8 +308,8 @@ void CARDUCode::HandleMail(u32 mail)
     INFO_LOG_FMT(DSPHLE, "ARAM work address: {:08x}", params.aram_work_addr);
     INFO_LOG_FMT(DSPHLE, "Output MRAM address: {:08x}", params.mram_output_addr);
 
-    // Call at 003d into ROM code
-    ProcessParameters(params);
+    // 003d - Call into ROM code
+    DoCardHash(params);
 
     // 003f - 0045: send a response.
     m_mail_handler.PushMail(DSP_DONE);
