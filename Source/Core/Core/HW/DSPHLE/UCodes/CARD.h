c1e0e32933a6113729fea60354634e9fa85327c8
Pokechu22 <Pokechu022@gmail.com>
2022-06-04 19:10:34 -0700

    Untested basic implementation

--- Source/Core/Core/HW/DSPHLE/UCodes/CARD.h
+++ Source/Core/Core/HW/DSPHLE/UCodes/CARD.h
@@ -34,11 +34,12 @@ private:
   // The addresses listed here are read and written by the DSP ROM only
   struct CardUcodeWorkData
   {
-    u32 work_0408;       // high: 0408, low: 0409
-    u32 final_response;  // high: 040a, low: 040b
-    u32 work_040c;       // high: 040c, low: 040d
-    u32 work_040e;       // high: 040e, low: 040f
-    u32 work_0410;       // high: 0410, low: 0411
+    u32 work_0408;  // high: 0408, low: 0409
+    u32 work_040a;  // high: 040a, low: 040b - serves as the final hash
+    u32 work_040c;  // high: 040c, low: 040d
+    u32 work_040e;  // high: 040e, low: 040f
+    u16 work_0410;
+    u16 work_0411;
   };
 
   enum class State
